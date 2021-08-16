5d21bb70d4f85ff7347ba196aa28f828281beda3
Kizeldur <75386872+Kizeldur@users.noreply.github.com>
2021-08-16 21:53:20 +0300

    Add files via upload

---
+++ Libra.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TranslatorLibrary
+{
+    public class Libra
+    {
+        public Dictionary<string, string> dictionary= new Dictionary<string, string>();
+
+        public void Add(string word, string translation)
+        {
+            dictionary.Add(word, translation);
+        }
+        
+        public void DictionaryFromFile(string path)
+        {
+            var doc = new StreamReader(path);
+            while (doc.Peek() >=0)
+            {
+                var str = doc.ReadLine();
+                var pair = str.Split(" - ");
+                Add(pair[0],pair[1]);
+            }
+        }
+
+        public void DictionaryFromJson(string path)
+        {
+            /*using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                var restoredPerson = JsonSerializer.Deserialize<(string key, string value)>(fs);
+                Console.WriteLine($"Name: {restoredPerson.Name}  Age: {restoredPerson.Age}");
+            }*/
+            
+        }
+
+        public async Task CreateJson()
+        {
+            using (FileStream fs = new FileStream("D:/newjsom.json", FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync<Dictionary<string, string>>(fs, dictionary);
+                Console.WriteLine("Data has been saved to file");
+            }
+        }
+    }
+}
---
+++ Translator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace TranslatorLibrary
+{
+    public static class Translator
+    {
+        public static string Translate(Dictionary<string, string> dictionary, string word)
+        {
+            string translation;
+            try
+            {
+                translation = dictionary[word];
+                return translation;
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new KeyNotFoundException();
+            }
+        }
+        
+        public static string ShowWholeDictionary(Dictionary<string, string> dictionary)
+        {
+            string dict = String.Empty;
+            foreach (var pair in dictionary)
+            {
+                dict += $"{pair.Key} - {pair.Value}";
+            }
+
+            return dict;
+        }
+
+        public static List<string> GiveMeKeys(Dictionary<string, string> dictionary)
+        {
+            var keyList = new List<string>();
+            foreach (var pair in dictionary)
+            {
+                keyList.Add(pair.Key);
+            }
+            return keyList;
+        }
+        
+        public static List<string> GiveMeValues(Dictionary<string, string> dictionary)
+        {
+            var valueList = new List<string>();
+            foreach (var pair in dictionary)
+            {
+                valueList.Add(pair.Value);
+            }
+            return valueList;
+        }
+    }
+}
